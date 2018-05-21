45e6de94f7bdefa6cd244097f1c1be6a1be4a7a2
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2018-05-21 10:17:21 +0800

    Added the new notification class and express object install method

--- controller.php
+++ controller.php
@@ -32,11 +32,77 @@ class Controller extends Package
         if (!is_object($type)) {
             $type = $factory->add('optional_value', 'Optional Value', $pkg);
         }
+        $this->installExpressObject();
     }
 
     public function on_start()
     {
         $this->app->make('Concrete\Core\Express\Controller\Manager')
                   ->setStandardController('\WorkhouseAdvertising\ExtendedExpressForms\Controller\FormController');
+    }
+
+    protected function installExpressObject()
+    {
+        // Register Express objects
+        // Check for and create the required express objects
+        $formNotificationObject = \Express::getObjectByHandle('form_notification');
+        // $existingObjects = Express::getEntities();
+        // foreach ($existingObjects as $existingObject) {
+        //     var_dump($existingObject->getHandle());
+        // }
+
+        //// TODO: Add a console command or something to handle updates to custom express objects
+        if (!$formNotificationObject) {
+            $package = null;
+            $formNotificationObject = \Express::buildObject('form_notification', 'form_notifications', 'Form Notification', $package);
+            // Set up a select multiple settings object
+            $selectMultipleSettings = new \Concrete\Core\Entity\Attribute\Key\Settings\SelectSettings();
+            $selectMultipleSettings->setAllowMultipleValues(true);
+            $selectMultipleSettings->setAllowOtherValues(true);
+            // Set options list
+            //// TODO: Figure out how to set these options or consider leaving it as-is but allowing users to specify options
+            // $entityManager = \Core::make('database/orm')->entityManager();
+            // $expressFormRepository = $entityManager->getRepository('Concrete\Core\Entity\Express\Form');
+            // $expressForms = $expressFormRepository->findAll();
+            // $selectOptions = [];
+            // foreach ($expressForms as $expressForm) {
+            //     $selectOptions[] = $expressForm->getEntity()->getHandle();
+            // }
+            // $optionsList = new \Concrete\Core\Entity\Attribute\Value\Value\SelectValueOptionList();
+            // $optionsList->setOptions($selectOptions);
+            // $selectMultipleSettings->setOptionList($optionsList);
+            // Add attributes
+            $formNotificationObject->addAttribute('text', 'BCC', 'form_notification_title');
+            $formNotificationObject->addAttribute('email', 'From Email', 'form_notification_from_email');
+            $formNotificationObject->addAttribute('text', 'From Name', 'form_notification_from_name');
+            $formNotificationObject->addAttribute('text', 'Subject', 'form_notification_subject');
+            $formNotificationObject->addAttribute('textarea', 'Content', 'form_notification_content');
+            $formNotificationObject->addAttribute('select', 'Applicable Forms', 'form_notification_forms', $selectMultipleSettings);
+            $formNotificationObject->addAttribute('email', 'Send To (Leave empty for autoresponder)', 'form_notification_to');
+            $formNotificationObject->addAttribute('email', 'Reply To', 'form_notification_reply_to');
+            $formNotificationObject->addAttribute('text', 'BCC', 'form_notification_bcc');
+            $formNotificationObject->addAttribute('select', 'Template', 'form_notification_template', $selectMultipleSettings);
+            $formNotificationObject->save();
+            //// TODO: See if DB rollback is automatic or if we need to implement it here
+            //// TODO: Automatically create an administration form too
+            $form = $formNotificationObject->buildForm('Form');
+            $form->addFieldset('Details')
+                // ->addTextControl('', 'This is just some basic explanatory text.')
+                ->addAttributeKeyControl('form_notification_title')
+                ->addAttributeKeyControl('form_notification_from_email')
+                ->addAttributeKeyControl('form_notification_from_name')
+                ->addAttributeKeyControl('form_notification_subject')
+                ->addAttributeKeyControl('form_notification_content')
+                ->addAttributeKeyControl('form_notification_forms')
+                ->addAttributeKeyControl('form_notification_to')
+                ->addAttributeKeyControl('form_notification_reply_to')
+                ->addAttributeKeyControl('form_notification_bcc')
+                ->addAttributeKeyControl('form_notification_template');
+            $form = $form->save();
+            // Set the default forms for the new object
+            $formNotificationObject->setDefaultViewForm($form);
+            $formNotificationObject->setDefaultEditForm($form);
+            $formNotificationObject->save();
+        }
     }
 }
--- src/WorkhouseAdvertising/ExtendedExpressForms/Express/Notification/FormBlockSubmissionEmailToSenderNotification.php
+++ src/WorkhouseAdvertising/ExtendedExpressForms/Express/Notification/FormBlockSubmissionEmailToSenderNotification.php
@@ -1,1 +1,84 @@
-FormBlockSubmissionEmailToSenderNotification.php
+<?php
+namespace WorkhouseAdvertising\ExtendedExpressForms\Express\Notification;
+
+use Express;
+use Concrete\Core\Entity\Express\Entry;
+use Concrete\Core\Express\Entry\Notifier\Notification\FormBlockSubmissionEmailNotification;
+use Concrete\Core\User\UserInfoRepository;
+use Doctrine\ORM\EntityManager;
+
+class FormBlockSubmissionEmailToSenderNotification extends FormBlockSubmissionEmailNotification
+{
+    protected function getReplyToEmail(Entry $entry)
+    {
+        return $this->getFromEmail();
+    }
+
+    protected function getToEmail(Entry $entry = null)
+    {
+        $toEmail = parent::getToEmail();
+        if ($entry) {
+           $toEmail = parent::getReplyToEmail($entry);
+        }
+        return $toEmail;
+    }
+
+    public function notify(Entry $entry, $updateType)
+    {
+        $notificationTriggered = false;
+        $form = $entry->getEntity();
+        $formHandle = $form->getHandle();
+        $customNotificationsObject = Express::getObjectByHandle('form_notification');
+        if ($customNotificationsObject) {
+            $customNotifications = $customNotificationsObject->getEntries();
+            foreach ($customNotifications as $customNotification) {
+                foreach ($customNotification->getFormNotificationForms()->getSelectedOptions() as $selectedOption) {
+                    if (trim($selectedOption->getSelectAttributeOptionValue()) == trim($formHandle)) {
+                        $notificationTriggered = true;
+                        $message = (trim($customNotification->getFormNotificationContent()))? $customNotification->getFormNotificationContent() : $this->blockController->thankyouMsg;
+                        $subject = (trim($customNotification->getFormNotificationSubject()))? $customNotification->getFormNotificationSubject() : t('Thank you for your enquiry');
+                        $fromAddress = (trim($customNotification->getFormNotificationFromEmail()))? trim($customNotification->getFormNotificationFromEmail()) : $this->getFromEmail();
+                        $fromName = (trim($customNotification->getFormNotificationFromName()))? trim($customNotification->getFormNotificationFromName()) : null;
+                        $toEmail = (trim($customNotification->getFormNotificationTo()))? trim($customNotification->getFormNotificationTo()) : $this->getToEmail($entry);
+                        $replyTo = (trim($customNotification->getFormNotificationReplyTo()))? trim($customNotification->getFormNotificationReplyTo()) : $this->getReplyToEmail($entry);
+                        $template = (trim($customNotification->getFormNotificationTemplate()))? trim($customNotification->getFormNotificationTemplate()) : 'block_express_form_submission_to_sender';
+                        $bcc = (trim($customNotification->getFormNotificationBcc()))? trim($customNotification->getFormNotificationBcc()) : false;
+                        $mh = $this->app->make('mail');
+                        $mh->to($toEmail);
+                        $mh->from($fromAddress, $fromName);
+                        $mh->replyto($replyTo);
+                        if ($bcc) {
+                            $mh->bcc($bcc);
+                        }
+                        $mh->addParameter('entity', $entry->getEntity());
+                        $mh->addParameter('formName', $this->getFormName($entry));
+                        $mh->addParameter('attributes', $this->getAttributeValues($entry));
+                        $mh->addParameter('message', $message);
+                        $mh->load($template);
+                        if (!$mh->getSubject()) {
+                            $mh->setSubject($subject);
+                        }
+                        $mh->sendMail();
+                    }
+                }
+            }
+        }
+        // if ($this->blockController->notifyMeOnSubmission && $this->blockController->replyToEmailControlID) {
+        // Only send the default one if a notification hasn't already been triggered
+        if ($this->blockController->replyToEmailControlID && !$notificationTriggered) {
+            $mh = $this->app->make('mail');
+            $mh->to($this->getToEmail($entry));
+            $mh->from($this->getFromEmail());
+            $mh->replyto($this->getReplyToEmail($entry));
+            $mh->addParameter('entity', $entry->getEntity());
+            $mh->addParameter('formName', $this->getFormName($entry));
+            $mh->addParameter('attributes', $this->getAttributeValues($entry));
+            $mh->addParameter('message', $this->blockController->thankyouMsg);
+            $mh->load('block_express_form_submission_to_sender');
+            if (!$mh->getSubject()) {
+                $mh->setSubject(t('Thank you for your enquiry'));
+            }
+            $mh->sendMail();
+        }
+    }
+}
